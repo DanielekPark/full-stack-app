733010308985b42256a1f6583a8ccb0a07920d8a
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2021-10-25 11:59:58 -0700

    added new files

--- client/src/context.js
+++ client/src/context.js
@@ -5,19 +5,19 @@ const ProvideContext = React.createContext();
 const AppProvider = ({children}) => {
   const [courses, setCourses] = React.useState([]);   
 
-  const api = 'http://localhost:5000/api';
+  // const api = 'http://localhost:5000/api';
 
-  const fetchCourses = async () => {
-    const data = await fetch(`${api}/courses`);
-    console.log(data);
-  }
+  // const fetchCourses = async () => {
+  //   const data = await fetch(`${api}/courses`);
+  //   console.log(data);
+  // }
 
   React.useEffect(() => {
-    fetchCourses(); 
+
   }, []);
 
   return (
-    <ProvideContext.Provider value={{fetchCourses, courses, setCourses}}>
+    <ProvideContext.Provider value={{courses, setCourses}}>
       {children}
     </ProvideContext.Provider>
   );
