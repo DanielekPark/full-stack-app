8745fd66b3a18383c66842e6ae891b61cbb55c6f
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2021-12-15 07:27:11 -0800

    changes to authentication

--- client/src/Components/UserSignIn.js
+++ client/src/Components/UserSignIn.js
@@ -3,22 +3,69 @@ import React from "react";
 import {ProvideContext, useProvideContext} from '../context';
 
 const UserSignIn = () => {
-  const {fetchData, cancelBtn, handleChange, userAccount, setUserAccount, isUserSignedIn, signIn, isSignedIn} = useProvideContext(ProvideContext);
+  const {cancelBtn, handleChange, userAccount, setUserAccount, isUserSignedIn, isSignedIn, setIsSignedIn} = useProvideContext(ProvideContext);
 
   //DELETE EITHER useProvideContext or isUserSignedIn
   //const {isSignedIn} = usePrivateRoute(); 
+  
+  // 1.
+  //how to add authorization in the fetch request (FRONT END)
+  //https://gist.github.com/ivermac/922def70ed9eaf83799b68ab1a587595  
+  /*
+  LOOK AT CLARK'S REPO
+  */
+
+  const fetchData = async (resource) => {
+    const url = `http://localhost:5001/api/${resource}`; 
+
+    const encodeCredentials = btoa(`${userAccount.emailAddress} : ${userAccount.password}`);
+
+    try {
+      const response = await fetch(url, 
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Basic ${encodeCredentials}`, 
+            'Content-Type': 'application/json'
+          }
+        }); 
+      return await response.json();
+    }catch (err) {
+      console.log(`There was a problem ${err}`); 
+    }
+  }
+  
+  const signIn = async (users) => {
+    if(!userAccount.emailAddress && !userAccount.password) return;
+    const checkEmail = await users.find((item) => item.emailAddress === userAccount.emailAddress); 
+    const checkPassword = await users.find((item) => item.password === userAccount.password);
+
+    //When signed in, the form is not displayed
+    if(!checkEmail || !checkPassword) {
+      alert('Please check your email and password');
+      return;
+    }else {
+      console.log(checkEmail)
+      localStorage.setItem('user', JSON.stringify(checkEmail));
+      setIsSignedIn(true);  
+    }
+  }  
   
   //GET REQUEST user email & password, FOR SIGNING-IN
   const handleSubmit = (event) => {
     event.preventDefault(); 
     fetchData('users')
-      .then((users) => signIn(users)); 
+      .then((users) => signIn(users))
   }
 
   React.useEffect(() => {
+    document.title = "Sign In"; 
     isUserSignedIn(); 
   }, [])  
 
+  React.useEffect(() => {
+    if(isSignedIn) window.location.href= "/"; 
+  }, [isSignedIn]); 
 
   //const if(!isSignedIn) return <Redirect to="/signin" />  
   return (
