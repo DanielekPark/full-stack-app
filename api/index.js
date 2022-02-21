7a4b638decc8b8e2791f6fab167640c185a955b4
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2022-02-21 14:47:25 -0800

    updated files and index.js

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const express = require("express");
 const app = express();
 const users = require("./routes/userRoute");
 const courses = require("./routes/courseRoute");
+const bodyParser = require('body-parser');
+const path = require('path');
 
 require("dotenv").config();
 
@@ -13,10 +15,16 @@ mongoose
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
 
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use("/api/users", users);
 app.use("/api/courses", courses);
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build'))
+})
+
 app.use("/", (req, res, next) => res.send("Hello World!"));
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
